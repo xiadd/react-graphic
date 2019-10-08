e99777daef12052c8d8ba2be55e77bf2983b8580
xiadd <xiadd0102@gmail.com>
2019-10-08 09:44:16 +0800

    docs:增加live demo

--- demo/src/index.js
+++ demo/src/index.js
@@ -1,27 +1,38 @@
 import React, {Component} from 'react'
+import { LiveProvider, LiveEditor, LiveError, LivePreview } from 'react-live'
 import {render} from 'react-dom'
 
 import ReactG from '../../src'
 
 const { Canvas, Group, Rect } = ReactG
 
+const scope = { Canvas, Group, Rect }
+
+const demoCode = `
+<Canvas id="c1" width={1000} height={1000}>
+  <Group>
+    <Rect 
+      cursor="pointer" 
+      x={100} 
+      y={100} 
+      width={100} 
+      height={100} 
+      fill="#d44131" 
+      onClick={(e) => console.log(e)}
+    />
+  </Group>
+</Canvas>
+`
+
 class Demo extends Component {
   render() {
-    return <div>
-      <h1>react-g Demo</h1>
-      <Canvas id="c1" width={1000} height={1000}>
-        <Group>
-          <Rect 
-            cursor="pointer" 
-            x={100} 
-            y={100} 
-            width={100} 
-            height={100} 
-            fill="red" 
-          />
-        </Group>
-      </Canvas>
-    </div>
+    return (
+      <LiveProvider code={demoCode} scope={scope}>
+        <LiveEditor />
+        <LiveError />
+        <LivePreview />
+      </LiveProvider>
+    )
   }
 }
 
